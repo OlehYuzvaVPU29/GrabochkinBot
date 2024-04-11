d955005bb81563501775f602321d72fe4d108fb6
Oleh <oleguzva3@gmail.com>
2024-04-11 11:48:21 +0300

    Updated Echo to message_handlers from command_handlers

--- handlers/command_handlers/__init__.py
+++ handlers/command_handlers/__init__.py
@@ -2,4 +2,3 @@ from .hello_handler import HelloHandler
 from .start_handler import StartHandler
 from .bye_handler import ByeHandler
 from .author_handler import AuthorHandler
-from .echo_handler import EchoHandler
